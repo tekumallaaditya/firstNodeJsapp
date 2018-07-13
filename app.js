56d66894a2cde484a4a0ed07c460605cb6c2ae4f
Aditya Tekumalla <tekumallaaditya@gmail.com>
2018-07-13 19:23:55 +0200

    corrections regarding port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response){
     response.sendFile(__dirname+ '/index.html');
 });
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 var server = app.listen(port, function(req, res){
     console.log('it works!')
